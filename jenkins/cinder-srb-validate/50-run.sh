3a6079607e6df087e311ca7f77aa8d651c95646a
Nicolas Trangez <ikke@nicolast.be>
2015-01-12 15:54:00 +0100

    Actually use SRB

--- jenkins/cinder-srb-validate/50-run.sh
+++ jenkins/cinder-srb-validate/50-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xue
 
+export MY_LVM_CONF=/etc/lvm/lvm.conf
+
 echo "Loading srb..."
 sudo insmod ./srb/srb.ko
 
@@ -21,16 +23,39 @@ export RE_EXEC=true
 export DEVSTACK_GATE_TEMPEST_REGEX=tempest.api.volume
 
 function pre_test_hook() {
+    local xtrace=$(set +o | grep xtrace)
+    local eerror=$(set +o | grep errexit)
+
+    set -o xtrace
+    set -o errexit
+
     echo "Running pre test hook"
 
-    ls /opt/stack
-    ps afx
+    echo "Updating LVM configuration"
+    sed -i 's/# types = \[\ "fd",\ 16\ \]/types = [ "srb", 16 ]/' $MY_LVM_CONF
 
     echo "Setting up devstack utility scripts"
+    cp jenkins/cinder-srb-validate/cinder_backends/srb /opt/stack/new/devstack/lib/cinder_backends/
+    cp jenkins/cinder-srb-validate/cinder_plugins/srb /opt/stack/new/devstack/lib/cinder_plugins/
 
-    echo "Loading SRB"
+    $eerror
+    $xtrace
 }
 export -f pre_test_hook
+
+DEVSTACK_LOCAL_CONFIG_FILE=$(mktemp)
+
+cat > $DEVSTACK_LOCAL_CONFIG_FILE << EOF
+CINDER_ENABLED_BACKENDS=srb:srb-1
+CINDER_SRB_BASE_URLS=http://$JCLOUDS_IPS:8000/
+
+TEMPEST_VOLUME_VENDOR='Scality'
+TEMPEST_STORAGE_PROTOCOL='Scality Rest Block Device'
+EOF
+
+export DEVSTACK_LOCAL_CONFIG=$(cat $DEVSTACK_LOCAL_CONFIG_FILE)
+
+rm $DEVSTACK_LOCAL_CONFIG_FILE
 
 ./devstack-gate/devstack-vm-gate-wrap.sh
 RC=$?
